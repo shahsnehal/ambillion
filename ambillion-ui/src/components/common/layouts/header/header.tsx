a46d372bd42795f2044b0dd5738e704553266ec1
VishalDevani <vishal.devani@techforceglobal.com>
2024-06-19 18:28:12 +0530

    implement initial header and sidebar

--- ambillion-ui/src/components/common/layouts/header/header.tsx
+++ ambillion-ui/src/components/common/layouts/header/header.tsx
@@ -1,3 +1,212 @@
+import { Icon } from '@iconify/react';
+import { ROUTES } from 'constants/common';
+import { useNavigate } from 'react-router-dom';
 export const Header = () => {
-    return <div>header</div>;
+    const navigate = useNavigate();
+    return (
+        <div className="page-wrapper">
+            <header className="topbar card rounded-0 border-0">
+                <div className="with-vertical">
+                    <nav className="navbar navbar-expand-lg px-0 py-0">
+                        <div className="d-none d-lg-block">
+                            <div className="brand-logo d-flex align-items-center justify-content-between">
+                                <a className="text-nowrap logo-img d-flex align-items-center gap-6">
+                                    <b className="logo-icon">
+                                        <i className="wi wi-sunset"></i>
+
+                                        <img
+                                            src="assets/images/logos/logo-icon.svg"
+                                            alt="homepage"
+                                            width={210}
+                                            className="light-logo"
+                                        />
+                                    </b>
+                                </a>
+                            </div>
+                        </div>
+                        <ul className="navbar-nav align-items-center">
+                            <li className="nav-item nav-icon-hover-bg rounded-circle">
+                                <a
+                                    className="nav-link sidebartoggler"
+                                    id="headerCollapse"
+                                    href="javascript:void(0)"
+                                >
+                                    <Icon icon="solar:list-broken" />
+                                </a>
+                            </li>
+                        </ul>
+                        <div className="d-block d-lg-none">
+                            <div className="brand-logo d-flex align-items-center justify-content-between">
+                                <a
+                                    href="javascript:void(0);"
+                                    className="text-nowrap logo-img d-flex align-items-center gap-6"
+                                >
+                                    <b className="logo-icon">
+                                        <i className="wi wi-sunset"></i>
+
+                                        <img
+                                            src="assets/images/logos/logo-icon.svg"
+                                            alt="homepage"
+                                            width={160}
+                                            className="light-logo"
+                                        />
+                                    </b>
+                                </a>
+                            </div>
+                        </div>
+                        <a
+                            className="navbar-toggler p-0 border-0 nav-icon-hover-bg rounded-circle"
+                            href="javascript:void(0)"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="p-2">
+                                <i className="ti ti-dots fs-7"></i>
+                            </span>
+                        </a>
+                        <div
+                            className="collapse navbar-collapse justify-content-end"
+                            id="navbarNav"
+                        >
+                            <div className="d-flex align-items-center justify-content-between">
+                                <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-center">
+                                    <li className="nav-item dropdown">
+                                        <a
+                                            className="nav-link"
+                                            href="javascript:void(0)"
+                                            id="drop2"
+                                            aria-expanded="false"
+                                        >
+                                            <img
+                                                src="assets/images/profile/user-1.jpg"
+                                                alt="user"
+                                                className="profile-pic rounded round-30"
+                                            />
+                                        </a>
+                                        <div
+                                            className="dropdown-menu pt-0 content-dd overflow-hidden pt-0 dropdown-menu-end user-dd"
+                                            aria-labelledby="drop2"
+                                        >
+                                            <div
+                                                className="profile-dropdown position-relative"
+                                                data-simplebar=""
+                                            >
+                                                <div className=" py-3 border-bottom">
+                                                    <div className="d-flex align-items-center px-3">
+                                                        <img
+                                                            src="assets/images/profile/user-1.jpg"
+                                                            className="rounded"
+                                                            width="50"
+                                                            height="50"
+                                                            alt="ample-img"
+                                                        />
+                                                        <div className="ms-3">
+                                                            <h5 className="mb-0 fs-4">
+                                                                James White
+                                                            </h5>
+                                                            <p className="mb-0 d-flex align-items-center text-muted">
+                                                                info@ambillion.com
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="message-body pb-3">
+                                                    <div className="px-3 pt-3">
+                                                        <div className="h6 mb-0 dropdown-item py-8 px-3 rounded-2 link">
+                                                            <a
+                                                                href="javascript:void(0)"
+                                                                className=" d-flex  align-items-center "
+                                                            >
+                                                                My Profile
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                    <hr />
+                                                    <div className="px-3">
+                                                        <div className="h6 mb-0 dropdown-item py-8 px-3 rounded-2 link">
+                                                            <a
+                                                                href="javascript:void(0)"
+                                                                className=" d-flex  align-items-center  "
+                                                            >
+                                                                Account Settings
+                                                            </a>
+                                                        </div>
+                                                        <div className="h6 mb-0 dropdown-item py-8 px-3 rounded-2 link">
+                                                            <a
+                                                                onClick={() =>
+                                                                    navigate(ROUTES.LOGIN)
+                                                                }
+                                                                className=" d-flex  align-items-center "
+                                                            >
+                                                                Sign Out
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            <aside className="left-sidebar with-horizontal">
+                <div>
+                    <nav id="sidebarnavh" className="sidebar-nav scroll-sidebar container-fluid">
+                        <ul id="sidebarnav">
+                            <li className="sidebar-item">
+                                <a
+                                    className="sidebar-link has-arrow"
+                                    href="javascript:void(0)"
+                                    aria-expanded="false"
+                                >
+                                    <span className="d-flex">
+                                        <Icon icon="solar:screencast-2-outline" className="fs-6" />
+                                    </span>
+                                    <span className="hide-menu">DASHBOARD</span>
+                                </a>
+                                <ul aria-expanded="false" className="collapse first-level">
+                                    <li className="sidebar-item">
+                                        <a
+                                            href="javascript:void(0)"
+                                            className="sidebar-link sublink"
+                                        >
+                                            <div className="round-16 d-flex align-items-center justify-content-center">
+                                                <Icon icon="solar:stop-circle-line-duotone" />
+                                            </div>
+                                            <span className="hide-menu">USER LIST</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-item">
+                                        <a href="index2.html" className="sidebar-link sublink">
+                                            <div className="round-16 d-flex align-items-center justify-content-center">
+                                                <Icon icon="solar:stop-circle-line-duotone" />
+                                            </div>
+                                            <span className="hide-menu">SERVICES</span>
+                                        </a>
+                                    </li>
+                                    <li className="sidebar-item">
+                                        <a
+                                            href="javascript:void(0)"
+                                            className="sidebar-link sublink"
+                                        >
+                                            <div className="round-16 d-flex align-items-center justify-content-center">
+                                                <Icon icon="solar:stop-circle-line-duotone" />
+                                            </div>
+                                            <span className="hide-menu">FAQ</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </aside>
+        </div>
+    );
 };
--- ambillion-ui/src/components/common/layouts/layouts.tsx
+++ ambillion-ui/src/components/common/layouts/layouts.tsx
@@ -1,15 +1,105 @@
 // import Header from './Header';
 // import SideBar from './SideBar';
 
+import { Icon } from '@iconify/react';
 import { Header } from './header/header';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const Layout = (children: any) => (
-    <div className="layout">
+export const Layout = () => (
+    <div id="main-wrapper">
+        <aside className="left-sidebar with-vertical">
+            <div>
+                <nav className="sidebar-nav scroll-sidebar" data-simplebar="">
+                    <ul id="sidebarnav">
+                        <li className="sidebar-item sidebar-profile pt-2">
+                            <a
+                                className="sidebar-link has-arrow opacity-100 gap-2"
+                                href="javascript:void(0)"
+                                aria-expanded="false"
+                            >
+                                <span className="d-flex">
+                                    <img
+                                        src="assets/images/profile/user-1.jpg"
+                                        className="rounded"
+                                        width="30"
+                                        alt="user"
+                                    />
+                                </span>
+                                <span className="hide-menu fw-medium">James White </span>
+                            </a>
+                        </li>
+                        <li className="nav-small-cap">
+                            <Icon icon="solar:menu-dots-bold" className="nav-small-cap-icon fs-4" />
+                            <span className="hide-menu">HOME</span>
+                        </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="" id="get-url">
+                                <Icon icon="solar:screencast-2-linear" className="aside-icon" />
+                                <span className="hide-menu">DASHBOARD</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="main/index2.html">
+                                <Icon
+                                    icon="solar:user-circle-line-duotone"
+                                    className="aside-icon"
+                                />
+                                <span className="hide-menu">USER LIST</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a
+                                className="sidebar-link"
+                                // href="main/page-faq.html"
+                                aria-expanded="false"
+                            >
+                                <div className="d-flex">
+                                    <Icon icon="solar:question-circle-linear" />
+                                </div>
+                                <span className="hide-menu">SERVICES</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a
+                                className="sidebar-link"
+                                // href="main/page-faq.html"
+                                aria-expanded="false"
+                            >
+                                <div className="d-flex">
+                                    <Icon icon="solar:question-circle-linear" />
+                                </div>
+                                <span className="hide-menu">FAQ</span>
+                            </a>
+                        </li>
+                        {/* <li className="sidebar-item">
+                            <a className="sidebar-link" href="main/index3.html">
+                                <Icon icon="solar:box-minimalistic-linear" className="aside-icon" />
+                                <span className="hide-menu">Analytical</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="main/index4.html">
+                                <Icon icon="solar:buildings-2-linear" className="aside-icon" />
+                                <span className="hide-menu">Campaign</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="main/index5.html">
+                                <Icon icon="solar:basketball-linear" className="aside-icon" />
+                                <span className="hide-menu">Modern</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="main/index6.html">
+                                <Icon icon="solar:cart-large-2-linear" className="aside-icon" />
+                                <span className="hide-menu">Ecommerce</span>
+                            </a>
+                        </li> */}
+                    </ul>
+                </nav>
+            </div>
+        </aside>
         <Header />
-        {/* <div className="container">
-            <div className="main-container">{children} </div>
-        </div> */}
     </div>
 );
 
